4a1ffac434d59249e604047095b90e9eb447de2f
robzor92 <robin.eric.andersson@gmail.com>
2017-07-12 11:22:45 +0200

    Add GPU and TensorFlow parsing options

--- launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
+++ launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
@@ -63,6 +63,12 @@ public class SparkLauncher {
   public static final String EXECUTOR_EXTRA_LIBRARY_PATH = "spark.executor.extraLibraryPath";
   /** Configuration key for the number of executor CPU cores. */
   public static final String EXECUTOR_CORES = "spark.executor.cores";
+  /** Configuration key for the number of executor CPU cores. */
+  public static final String EXECUTOR_GPUS = "spark.executor.gpus";
+
+  static final String IS_TENSORFLOW = "spark.tensorflow.application";
+
+  static final String NUM_TENSORFLOW_PS = "spark.tensorflow.num.ps";
 
   static final String PYSPARK_DRIVER_PYTHON = "spark.pyspark.driver.python";
 
