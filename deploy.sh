3e21d0f798e2f29cb610abcfb26698fb70a23e40
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-03-07 14:54:13 +0100

    add hops deploy script

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+#./build/mvn -Phadoop-provided -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.3 -DskipTests clean package
+
+set -e
+
+VERSION=2.3.0
+HADOOP_PROFILE=2.8
+#HADOOP_VERSION=2.7.3
+#SCALA_VERSION=2.7
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ] ; then
+  echo "Usage: $0 [deploy]"
+  exit 0
+fi
+
+if [ $# -gt 1 ] || [ "$1" != "deploy" ] ; then
+    echo "Usage: $0 [deploy]"
+    exit 1
+fi
+
+
+./dev/make-distribution.sh --name "without-hadoop" --tgz "-Pyarn,hadoop-provided,hadoop-${HADOOP_PROFILE},parquet-provided"
+
+if [ "$1" == "deploy" ] ; then
+   echo "scp spark-${VERSION}-bin-without-hadoop.tgz to  glassfish@snurran.sics.se:/var/www/hops"
+   scp spark-${VERSION}-bin-without-hadoop.tgz glassfish@snurran.sics.se:/var/www/hops
+fi
+
